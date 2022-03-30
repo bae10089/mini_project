07f90f1ac79f9547edaa67296194a98f4b0cace7
bae10089 <bae10089@naver.com>
2022-03-31 01:41:12 +0900

    12

--- src/project_3.java
+++ src/project_3.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
 public class project_3 {
     public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.println("[입장권 계산]");
+        System.out.print("나이를 입력해 주세요.(숫자):");
+        int a = sc.nextInt();
+        System.out.print("입장시간을 입력해 주세요.(숫자입력):");
+        int b = sc.nextInt();
+        System.out.print("국가유공자 여부를 입력해 주세요.(y/n):");
+        String c = sc.nextLine();
+        System.out.print("복지카드 여부를 입력해 주세요.(y/n):");
+        String d = sc.nextLine();
 
     }
 }
