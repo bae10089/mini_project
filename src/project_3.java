c4abfbdb9368334301cd951c4d95d92d673953ee
bae10089 <bae10089@naver.com>
2022-03-31 01:59:16 +0900

    12

--- src/project_3.java
+++ src/project_3.java
@@ -8,9 +8,27 @@ public class project_3 {
         System.out.print("입장시간을 입력해 주세요.(숫자입력):");
         int b = sc.nextInt();
         System.out.print("국가유공자 여부를 입력해 주세요.(y/n):");
-        String c = sc.nextLine();
+        String s1 = sc.next();
+        char c =s1.charAt(0);
         System.out.print("복지카드 여부를 입력해 주세요.(y/n):");
-        String d = sc.nextLine();
-
+        String s2 = sc.next();
+        char d =s2.charAt(0);
+        int e = 0;
+        if(a<3){
+            e=0;
+        }
+        else if (c=='y'|| d=='y'){
+            e=8000;
+        }
+        else if (a<13){
+            e=4000;
+        }
+        else if (b>17){
+            e=4000;
+        }
+        else {
+            e=10000;
+        }
+        System.out.println("입장료:"+e);
     }
 }
