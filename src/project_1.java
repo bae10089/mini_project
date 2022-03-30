26b75cd1cbe429f2b10f0595b29027c213a0745e
bae10089 <bae10089@naver.com>
2022-03-31 01:15:24 +0900

    12

--- src/project_1.java
+++ src/project_1.java
@@ -1,8 +1,16 @@
+import java.sql.SQLOutput;
+
 public class project_1 {
     public static void main(String[] args){
-
-        System.out.println("Hello World");
-        System.out.println("Hello World");
-        System.out.println("Hello World");
+        System.out.println("[구구단 출력]");
+        for (int i=1; i<10; i++){
+            for (int j=1; j<10; j++){
+                String a = String.format("%02d",i);
+                String b = String.format("%02d",j);
+                String c = String.format("%02d",i*j);
+                System.out.print(b+" x "+a+" = "+c+"\t");
+            }
+            System.out.println();
+        }
     }
 }
